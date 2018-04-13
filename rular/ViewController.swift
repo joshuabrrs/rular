9c74ecd4e9560819410cce842632cfdc74ba729d
joshuabrrs <jgb16@my.fsu.edu>
2018-04-13 18:33:43 -0400

    .

---
+++ rular/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  rular
+//
+//  Created by Joshua Barrios on 4/13/18.
+//  Copyright © 2018 Joshua Barrios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginButton.layer.cornerRadius = 25
+        signupButton.layer.cornerRadius = 25
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
