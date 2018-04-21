8ed3b875bc14d12075e9b3f87e261605d3cacea0
joshuabrrs <jgb16@my.fsu.edu>
2018-04-21 17:56:43 -0400

    some comments

--- rular/AppDelegate.swift
+++ rular/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        
+        //this is where auth could be
         let myDB = Database.database().reference()
         myDB.setValue("it works!")
         
